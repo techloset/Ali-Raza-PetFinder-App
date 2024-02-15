9fd6e1174b6f0eba324fbfe766a9a398ad3d65a8
Ali Raza Gill <aliraza137725@gmail.com>
2024-02-15 18:34:19 +0500

    put the secret in the env file

--- src/utilities/axiosInstance.ts
+++ src/utilities/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 
  const fetchAccessToken = async () => {
     const clientId = "SlSGfGACJ8b7AQnXRhptI5ielIFxGiJCqbqy9YCi7l7SfynMU1";
-      const clientSecret = "54mZprlj29cDYzGuJqduStxRQ3SGg1vnTsPvYFul";
+      const clientSecret = process.env.REACT_APP_API_KEY;;
 
     try {
       const response = await axios.post('https://api.petfinder.com/v2/oauth2/token', {
