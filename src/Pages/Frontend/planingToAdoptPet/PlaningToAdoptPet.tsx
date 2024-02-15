8db79b2ad4b7216d5ce4d30ef6581237f244b517
Ali Raza Gill <aliraza137725@gmail.com>
2024-02-15 17:56:37 +0500

    error is solved

--- src/Pages/Frontend/planingToAdoptPet/PlaningToAdoptPet.tsx
+++ src/Pages/Frontend/planingToAdoptPet/PlaningToAdoptPet.tsx
@@ -37,28 +37,26 @@ export default function PlaningToAdoptPet() {
                 Learn More
               </button>
             </div>
-
-            <div className="flex flex-col  justify-center items-center text-center h-[280px]  max-w-[300px]">
-              <div className="w-[100px] h-[100px] my-[19px]">
-                <img src={plandogimg} alt="" className="object-cover" />
-              </div>
-              <div className="h-[26px] w-full flex-shrink text-purp mb-[19px]">
-                Pet Adoption FAQs
-              </div>
-              <div className="h-[45px] w-full flex flex-wrap mb-[19px] ">
-                Get answer to all the questions you have not thought of for your
-                adoption .
-              </div>
-              <div className="w-[138px] h-[38px] flex justify-center items-center border-2 rounded-3xl  mt-5 border-purp hover:scale-105 hover:bg-purp  text-purp hover:text-white ">
-                <button className=" text-sm w-[97px] h-[118px] ">
-                  Learn More
-                </button>
-              </div>
+          </div>
+          <div className="flex flex-col  justify-center items-center text-center h-[280px]  max-w-[300px]">
+            <div className="w-[100px] h-[100px] my-[19px]">
+              <img src={plandogimg} alt="" className="object-cover" />{" "}
+            </div>
+            <div className="h-[26px] w-full flex-shrink text-purp mb-[19px]">
+              Pet Adoption FAQs
+            </div>
+            <div className="h-[45px] w-full flex flex-wrap mb-[19px] ">
+              Get answer to all the questions you have not thought of for your
+              adoption .
+            </div>
+            <div className="w-[138px] h-[38px] flex justify-center items-center border-2 rounded-3xl  mt-5 border-purp hover:scale-105 hover:bg-purp  text-purp hover:text-white ">
+              <button className=" text-sm w-[97px] h-[118px] ">
+                Learn More
+              </button>
             </div>
           </div>
         </div>
       </div>
-      );
     </>
   );
 }
